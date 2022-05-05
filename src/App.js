09e58b9e8522f619d85102fc52e3dfe439893c9d
Bartlomiej Ciupa <bartek-ciupa@o2.pl>
2022-05-05 17:02:06 +0200

    issue resolved: added import fontello to App.js + intro gradient propose

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import Banner from './components/layout/banner/Banner'
-import Footer from './components/layout/footer/Footer'
-import Header from './components/layout/header/Header'
-import './css/Layout.css'
-import CustomerPortal from './customerportal/CustomerPortal'
-import Contact from './pages/Contact'
-import Home from './pages/Home'
-import Offer from './pages/Offer'
-import PriceList from './pages/PriceList'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Banner from './components/layout/banner/Banner';
+import Footer from './components/layout/footer/Footer';
+import Header from './components/layout/header/Header';
+import './css/Layout.css';
+import CustomerPortal from './customerportal/CustomerPortal';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Offer from './pages/Offer';
+import PriceList from './pages/PriceList';
+import './fontello/css/fontello.css';
 
 function App() {
-	return (
-		<div className='App'>
-			<Router>
-				<Header />
+  return (
+    <div className="App">
+      <Router>
+        <Header />
 
-				<div className='container'>
-					<Routes>
-						<Route path='/home' element={<Home />} />
-						<Route path='/offer' element={<Offer />} />
-						<Route path='/priceList' element={<PriceList />} />
-						<Route path='/customerPortal' element={<CustomerPortal />} />
-						<Route path='/contact' element={<Contact />} />
-					</Routes>
-				</div>
-				<Footer />
-
-			</Router>
-		</div>
-	)
+        <div className="container">
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/offer" element={<Offer />} />
+            <Route path="/priceList" element={<PriceList />} />
+            <Route path="/customerPortal" element={<CustomerPortal />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+        <Footer />
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
