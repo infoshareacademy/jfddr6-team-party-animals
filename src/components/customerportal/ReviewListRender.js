4c171bc4e5415df47dd0f88743ce82fe5b1686d7
Tomasz Paszkowski <tpaszkowski@poczta.fm>
2022-05-11 22:07:27 +0200

    finished ReviewList functionality

--- src/components/customerportal/ReviewListRender.js
+++ src/components/customerportal/ReviewListRender.js
@@ -1,58 +1,77 @@
 import { onAuthStateChanged, signOut } from "firebase/auth";
 import { useEffect, useState } from "react";
-import { getDoc, doc } from "firebase/firestore";
+import {
+  getDoc,
+  doc,
+  collection,
+  CollectionReference,
+  query,
+  getDocs,
+} from "firebase/firestore";
 import { auth, db } from "./../../db";
 
 const ReviewListRender = () => {
   const signOutUser = () => {
     signOut(auth);
   };
-  const [reviews, setReviews] = useState([]);
-  const [users, setUsers] = useState([]);
-
-  const getReviews = async (docId) => {
-    const docReference = doc(db, "reviews", docId);
-    const userDocument = await getDoc(docReference);
-
-    const user = {
-      id: userDocument.id,
-      data: userDocument.data(),
-    };
-
-    setUsers(user.data.name);
-    setReviews(user.data.reviews);
+  const defaultReviews = {
+    myReviews: [],
+    othersReviews: [],
   };
+  const [reviews, setReviews] = useState(defaultReviews);
 
+  const getAllReviews = async (id) => {
+    const collectionReference = collection(db, "reviews");
+    const reviewsDocs = await getDocs(collectionReference);
+    const allReviews = reviewsDocs.docs.reduce((a, doc) => {
+      return id === doc.id
+        ? { ...a, myReviews: [...a.myReviews, doc.data()] }
+        : { ...a, othersReviews: [...a.othersReviews, doc.data()] };
+    }, defaultReviews);
+    console.log(allReviews);
+    setReviews(allReviews);
+  };
   useEffect(() => {
     onAuthStateChanged(auth, (jwt) => {
       if (jwt === null) {
         return;
       }
       console.log("jwt", jwt.uid);
-      getReviews(jwt.uid);
+      getAllReviews(jwt.uid);
     });
   }, []);
-  const renderReviews = () =>
-    reviews.map((review) => {
+  const renderReviews = (reviews) => {
+    console.log(reviews);
+    return reviews.map((review) => {
       return (
-        <div key={review.date + review.petname}>
-          <span>
-            <p>Date: {review.date}</p>
-            <h3>Your groomer: {review.groomer}</h3>
-            <h3>Your name: {review.ownername}</h3>
-            <h3>Pets name: {review.petname}</h3>
-            <h3>Review: {review.reviewtext}</h3>
-            <h3>Rating: {review.rating}</h3>
-          </span>
-        </div>
+        <>
+          <p>Client name:{review.name}</p>
+          {review.reviews.map((review) => {
+            return (
+              <div key={review.date + review.petname}>
+                <span>
+                  <p>Date: {review.date}</p>
+                  <h3>Groomer name: {review.groomer}</h3>
+                  <h3>Owner name: {review.ownername}</h3>
+                  <h3>Pets name: {review.petname}</h3>
+                  <h3>Review: {review.reviewtext}</h3>
+                  <h3>Rating: {review.rating}</h3>
+                </span>
+              </div>
+            );
+          })}
+        </>
       );
     });
+  };
 
   return (
     <div>
-      <h2>You are logged in as: {users}</h2>
+      <h2>You are logged in as: </h2>
       <h2>Your reviews:</h2>
-      {renderReviews()}
+      {renderReviews(reviews.myReviews)}
+      <h2>All reviews:</h2>
+      {renderReviews(reviews.othersReviews)}
       <div>
         <button onClick={signOutUser}>Wyloguj się</button>
       </div>
