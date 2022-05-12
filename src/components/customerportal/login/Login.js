c9145964b1390161d4cc6f4722e6782606765d5f
Leena2405 <zaneta.lenart@gmail.com>
2022-05-12 17:55:32 +0200

    rename button and title in login

--- src/components/customerportal/login/Login.js
+++ src/components/customerportal/login/Login.js
@@ -1,70 +1,70 @@
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../../../db";
-import ResetPassword from "../resetpassword/ResetPassword";
+import { signInWithEmailAndPassword } from 'firebase/auth'
+import { auth } from '../../../db'
+import ResetPassword from '../resetpassword/ResetPassword'
 import {
-  FormColumn,
-  FormWrapper,
-  FormInput,
-  FormSection,
-  FormRow,
-  FormTitle,
-  FormLabel,
-  FormInputRow,
-  FormButton,
-} from "./LoginStyles";
-import { Container } from "../../../globalStyles";
-import React, { useState, useEffect, useMemo } from "react";
-import { useNavigate } from "react-router-dom";
+	FormColumn,
+	FormWrapper,
+	FormInput,
+	FormSection,
+	FormRow,
+	FormTitle,
+	FormLabel,
+	FormInputRow,
+	FormButton,
+} from './LoginStyles'
+import { Container } from '../../../globalStyles'
+import React, { useState, useEffect, useMemo } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
-  const [inputEmail, setInputEmail] = useState("");
-  const [inputPassword, setInputPassword] = useState("");
-  const navigate = useNavigate();
+	const [inputEmail, setInputEmail] = useState('')
+	const [inputPassword, setInputPassword] = useState('')
+	const navigate = useNavigate()
 
-  const loginUser = async (email, password) => {
-    await signInWithEmailAndPassword(auth, email, password).then((response) => {
-      console.log("login", response, response.user);
-      setInputEmail("");
-      setInputPassword("");
-    });
-  };
+	const loginUser = async (email, password) => {
+		await signInWithEmailAndPassword(auth, email, password).then(response => {
+			console.log('login', response, response.user)
+			setInputEmail('')
+			setInputPassword('')
+		})
+	}
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    loginUser(inputEmail, inputPassword);
-    navigate("/panel");
-  };
+	const handleSubmit = e => {
+		e.preventDefault()
+		loginUser(inputEmail, inputPassword)
+		navigate('/panel')
+	}
 
-  return (
-    <FormSection>
-      <Container>
-        <FormRow>
-          <FormColumn>
-            <FormTitle>Sign Up</FormTitle>
-            <FormWrapper onSubmit={handleSubmit}>
-              <FormInputRow>
-                <FormInput
-                  type="text"
-                  value={inputEmail}
-                  placeholder="Email"
-                  onChange={(e) => setInputEmail(e.target.value)}
-                />
-                <FormInput
-                  type="password"
-                  value={inputPassword}
-                  placeholder="Password"
-                  onChange={(e) => setInputPassword(e.target.value)}
-                />
-              </FormInputRow>
-              <FormButton type="submit">Sign Up</FormButton>
-              <FormButton type="submit">Sign Out</FormButton>
-            </FormWrapper>
-            <ResetPassword />
-          </FormColumn>
-        </FormRow>
-      </Container>
-    </FormSection>
-  );
-};
+	return (
+		<FormSection>
+			<Container>
+				<FormRow>
+					<FormColumn>
+						<FormTitle>Jump To Your Account </FormTitle>
+						<FormWrapper onSubmit={handleSubmit}>
+							<FormInputRow>
+								<FormInput
+									type='text'
+									value={inputEmail}
+									placeholder='Email'
+									onChange={e => setInputEmail(e.target.value)}
+								/>
+								<FormInput
+									type='password'
+									value={inputPassword}
+									placeholder='Password'
+									onChange={e => setInputPassword(e.target.value)}
+								/>
+							</FormInputRow>
+							<FormButton type='submit'>Log In </FormButton>
+							<FormButton type='submit'>Log Out</FormButton>
+						</FormWrapper>
+						<ResetPassword />
+					</FormColumn>
+				</FormRow>
+			</Container>
+		</FormSection>
+	)
+}
 
-export default Login;
+export default Login
